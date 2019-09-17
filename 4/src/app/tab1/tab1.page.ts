b7be3afe1f799425ca9561a5964329602c7ba1fe
anhnguyent93 <anh.nguyent93@gmail.com>
2019-09-16 20:19:31 -0500

    Clean the screen when searching

--- 4/src/app/tab1/tab1.page.ts
+++ 4/src/app/tab1/tab1.page.ts
@@ -19,27 +19,27 @@ export class Tab1Page {
   }
 
   search() {
+    this.resultImages = null;
+    this.resultKnowledge = null;
     if (this.input !== '') {
-      console.log(this.input);
       this.httpClient.get('https://kgsearch.googleapis.com/v1/entities:search?query=' + this.input +
           '&key=AIzaSyBwBVJOXIRpF0WaFEttaF9bLApiH2WftQQ&limit=1&indent=True')
           .subscribe((data: any) => {
             if (data.itemListElement.length !== 0) {
               this.resultKnowledge = data.itemListElement[0].result;
-              console.log(this.resultKnowledge);
             }
           });
     }
   }
 
   searchImage() {
+    this.resultImages = null;
+    this.resultKnowledge = null;
     if (this.input !== '') {
-      console.log(this.input);
       this.httpClient.get('https://pixabay.com/api/?key=13649062-5df1c82a676a823d5924d2b2b&q=' + this.input)
           .subscribe((data: any) => {
               if (data.hits.length !== 0) {
               this.resultImages = data.hits;
-              console.log(this.resultImages);
             }
           });
     }
